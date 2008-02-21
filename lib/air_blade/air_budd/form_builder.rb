885bbae8dca88131b375a83ae9ca6827fe314d8f
Andy Stewart <boss@airbladesoftware.com>
2008-02-21 17:46:39 +0000

    Use :label instead of :value to specify a field's label.

--- lib/air_blade/air_budd/form_builder.rb
+++ lib/air_blade/air_budd/form_builder.rb
@@ -8,10 +8,10 @@ module AirBlade
       # Creates a glorified form field helper.  It takes a form helper's usual
       # arguments with an optional options hash:
       #
-      # <%= form.text_field 'title', :required => true, :name => "Article's Title" %>
+      # <%= form.text_field 'title', :required => true, :label => "Article's Title" %>
       #
       # The code above generates the following HTML.  The :required entry in the hash
-      # triggers the <em/> element and the :name overwrites the default field name,
+      # triggers the <em/> element and the :label overwrites the default field label,
       # 'title' in this case, with its value.  The stanza is wrapped in a <p/> element.
       #
       # <p>
@@ -106,6 +106,8 @@ module AirBlade
         attributes_for_element = {:class => nature}.merge(element == :button  ?
                                                           {:type => 'submit'} :
                                                           {:href => (options[:url] || '')} )
+        # TODO: separate button and link construction and use
+        # link_to to gain its functionality, e.g. :back?
         @template.content_tag(element.to_s,
                               legend,
                               attributes_for_element)
@@ -124,9 +126,9 @@ module AirBlade
       # Writes out a <label/> element for the given field.
       # Options:
       #  - :required: true if field is mandatory, false otherwise (default)
-      #  - :value: text wrapped by the <label/>.  Optional (default is field's name).
+      #  - :label: text wrapped by the <label/>.  Optional (default is field's name).
       def label_element(field, options = {}, html_options = {})
-        value = "#{options.delete(:value) || field.to_s.humanize}:"
+        value = "#{options.delete(:label) || field.to_s.humanize}:"
         value += ' <em class="required">(required)</em>' if options.delete(:required)
 
         html_options.stringify_keys!
